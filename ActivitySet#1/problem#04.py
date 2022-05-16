fca81dd746a8824f204a1a77b6f9313831e5bdda
Shivani12309 <94678017+Shivaniii529@users.noreply.github.com>
2022-05-16 06:57:44 +0000

    ActivitySet#1/problem#04.py

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,14 @@
-# Conditional Execution
 
-hrs = input("Enter hours? ")
+def computepay(h,r):
+  if h<40:
+    pay= h*r
+  elif h>40:
+     pay = 40*r+(h-40)*1.5*r
+        
+     return pay
+  
+h = float(input("Enter hours? "))
+r = float(input("Enter rate per hour? "))
+    
+pay = computepay(h, r)
+print(pay)
